b7edc2b1a7e16c66c8b90fbafdb3bfdeabb0f3ec
petitesize <petitesize22@gmail.com>
2024-08-09 14:49:53 +0900

    Feat: 검색 기능 구현

--- src/components/common/searchBar/CommonSearchBar.tsx
+++ src/components/common/searchBar/CommonSearchBar.tsx
@@ -1,6 +1,33 @@
+import { useState } from "react";
 import styles from "./CommonSearchBar.module.scss";
+import { useRecoilState } from "recoil";
+import { searchState } from "@/recoil/atoms/searchState";
 
 function CommonSearchBar() {
+  const [search, setSearch] = useRecoilState(searchState);
+  const [text, setText] = useState("");
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+  const onSearch = () => {
+    if (text === "") {
+      // input 태그 안에 빈 값으로 검색하였을 때 = default value
+      setSearch("Korea");
+    } else {
+      setSearch(text);
+    }
+  };
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      if (text === "") {
+        // input 태그 안에 빈 값으로 검색하였을 때 = default value
+        setSearch("Korea");
+      } else {
+        setSearch(text);
+      }
+    }
+  };
+
   return (
     <div className={styles.searchBar}>
       <div className={styles.searchBar__search}>
@@ -8,8 +35,11 @@ function CommonSearchBar() {
           type="text"
           placeholder="찾으실 이미지를 검색하세요"
           className={styles.searchBar__search__input}
+          onChange={onChange}
+          value={text}
+          onKeyDown={handleKeyDown}
         />
-        <img src="src/assets/icons/icon-search.svg" alt="" />
+        <img src="src/assets/icons/icon-search.svg" alt="" onClick={onSearch} />
       </div>
     </div>
   );
